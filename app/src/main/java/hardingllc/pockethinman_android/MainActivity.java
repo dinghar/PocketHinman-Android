d3026a1339f12b5fb780ea563a5123d20b5e9cb8
Ross Harding <rnh8wb@virginia.edu>
2018-06-04 11:12:21 -0400

    Working on fixing camera preview aspect ratio

--- app/src/main/java/hardingllc/pockethinman_android/MainActivity.java
+++ app/src/main/java/hardingllc/pockethinman_android/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -48,6 +49,7 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -140,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
 
         formatSlider();
         formatZoomView();
-
+        formatImageView();
     }
 
 
@@ -169,6 +171,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void formatZoomView() {
         ZoomableViewGroup zoomableViewGroup = (ZoomableViewGroup) findViewById(R.id.zoomableViewGroup);
+    }
+
+    public void formatImageView() {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        Float imageAlpha = sharedPref.getFloat("imageAlpha", 0.5f);
+        imageView.setAlpha(imageAlpha);
+    }
+
+    public String getImageAlpha() {
+        Float imageAlpha = imageView.getAlpha();
+        return String.format("%.1f", imageAlpha);
+    }
+
+    public void setImageAlpha(Float newImageAlpha) {
+        imageView.setAlpha(newImageAlpha);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putFloat("imageAlpha", newImageAlpha);
+        editor.commit();
     }
 
     public void formatSlider() {
@@ -213,8 +234,45 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (popupWindow == null) {
                     LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-                    View calloutView = layoutInflater.inflate(R.layout.settings_callout, null);
+                    final View calloutView = layoutInflater.inflate(R.layout.settings_callout, null);
                     popupWindow = new PopupWindow(calloutView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    TextView alphaLabel = (TextView) calloutView.findViewById(R.id.alphaLabel);
+                    alphaLabel.setText("Alpha: " + getImageAlpha());
+                    Button alphaStepperMinusButton = (Button) calloutView.findViewById(R.id.alphaStepperMinus);
+                    alphaStepperMinusButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Float oldImageAlpha = imageView.getAlpha();
+                            Float newImageAlpha = oldImageAlpha - 0.1f;
+                            if (newImageAlpha < 0f) {
+                                newImageAlpha = 0f;
+                            }
+                            setImageAlpha(newImageAlpha);
+                            TextView alphaLabel = (TextView) calloutView.findViewById(R.id.alphaLabel);
+                            alphaLabel.setText("Alpha: " + getImageAlpha());
+                        }
+                    });
+                    Button alphaStepperPlusButton = (Button) calloutView.findViewById(R.id.alphaStepperPlus);
+                    alphaStepperPlusButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Float oldImageAlpha = imageView.getAlpha();
+                            Float newImageAlpha = oldImageAlpha + 0.1f;
+                            if (newImageAlpha > 1f) {
+                                newImageAlpha = 1f;
+                            }
+                            setImageAlpha(newImageAlpha);
+                            TextView alphaLabel = (TextView) calloutView.findViewById(R.id.alphaLabel);
+                            alphaLabel.setText("Alpha: " + getImageAlpha());
+                        }
+                    });
+                    Button panResizeButton = (Button) calloutView.findViewById(R.id.panResizeButton);
+                    panResizeButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(MainActivity.this, PanAndResizeActivity.class));
+                        }
+                    });
                     if(Build.VERSION.SDK_INT>=21){
                         popupWindow.setElevation(5.0f);
                     }
@@ -232,7 +290,6 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                mCamera.takePicture(null, null, mPicture);
                 takePicture();
             }
         });
@@ -344,13 +401,11 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
-//                    Toast.makeText(MainActivity.this, "Saved " + file, Toast.LENGTH_SHORT).show();
                     runOnUiThread(new Runnable(){
                         public void run() {
                             Log.d("asdf", file.getAbsolutePath());
                             Intent mediaStoreUpdateIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                             mediaStoreUpdateIntent.setData(Uri.fromFile(file));
-//                            imageView.setImageBitmap(BitmapFactory.decodeFile(file.getAbsolutePath()));
                             imageView.setImageURI(Uri.fromFile(file));
                         }
                     });
@@ -512,138 +567,6 @@ public class MainActivity extends AppCompatActivity {
         backgroundThread.start();
         backgroundHandler = new Handler(backgroundThread.getLooper());
     }
-
-    //
-//    private void configureCamera() {
-//        mCamera = getCameraInstance();
-//
-//        mCameraPreview = new CameraPreview(this, mCamera);
-//        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
-//        preview.addView(mCameraPreview);
-//    }
-//
-//    public static Camera getCameraInstance(){
-//
-//        Camera c = null;
-//        try {
-//            c = Camera.open(0); // attempt to get a Camera instance
-//        }
-//        catch (Exception e){
-//            // Camera is not available (in use or does not exist)
-//        }
-//        return c; // returns null if mCamera is unavailable
-//    }
-//
-//    private boolean checkCameraPermission() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    private void requestCameraPermission() {
-//
-//        ActivityCompat.requestPermissions(this,
-//                new String[]{Manifest.permission.CAMERA},
-//                PERMISSION_REQUEST_CODE);
-//
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-//        switch (requestCode) {
-//            case PERMISSION_REQUEST_CODE:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
-//                    configureCamera();
-//                } else {
-//                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-//                                != PackageManager.PERMISSION_GRANTED) {
-//                            showMessageOKCancel("You need to allow access permissions",
-//                                    new DialogInterface.OnClickListener() {
-//                                        @Override
-//                                        public void onClick(DialogInterface dialog, int which) {
-//                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                                                requestCameraPermission();
-//                                            }
-//                                        }
-//                                    });
-//                        }
-//                    }
-//
-//                }
-//                break;
-//        }
-//
-//    }
-//
-//    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-//        new AlertDialog.Builder(MainActivity.this)
-//                .setMessage(message)
-//                .setPositiveButton("OK", okListener)
-//                .setNegativeButton("Cancel", null)
-//                .create()
-//                .show();
-//    }
-//
-//    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-//
-//        @Override
-//        public void onPictureTaken(byte[] data, Camera camera) {
-//
-//            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-//            if (pictureFile == null){
-//                Log.d("2", "Error creating media file");
-//                return;
-//            }
-//
-//            try {
-//                FileOutputStream fos = new FileOutputStream(pictureFile);
-//                fos.write(data);
-//                fos.close();
-//            } catch (FileNotFoundException e) {
-//                Log.d("2", "File not found: " + e.getMessage());
-//            } catch (IOException e) {
-//                Log.d("2", "Error accessing file: " + e.getMessage());
-//            }
-//        }
-//    };
-//
-//    private static File getOutputMediaFile(int type){
-//        // To be safe, you should check that the SDCard is mounted
-//        // using Environment.getExternalStorageState() before doing this.
-//
-//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-//                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-//        // This location works best if you want the created images to be shared
-//        // between applications and persist after your app has been uninstalled.
-//
-//        // Create the storage directory if it does not exist
-//        if (! mediaStorageDir.exists()){
-//            if (! mediaStorageDir.mkdirs()){
-//                Log.d("MyCameraApp", "failed to create directory");
-//                return null;
-//            }
-//        }
-//
-//        // Create a media file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        File mediaFile;
-//        if (type == MEDIA_TYPE_IMAGE){
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "IMG_"+ timeStamp + ".jpg");
-//        } else if(type == MEDIA_TYPE_VIDEO) {
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "VID_"+ timeStamp + ".mp4");
-//        } else {
-//            return null;
-//        }
-//
-//        return mediaFile;
-//    }
 
 
 }
