7beb4ca5dfbfc28ff2b5c2ffd16a28349df9d261
Ross Harding <rnh8wb@virginia.edu>
2018-06-04 11:12:57 -0400

    Adding pan to resize functionality

---
+++ app/src/main/java/hardingllc/pockethinman_android/PanAndResizeActivity.java
@@ -0,0 +1,13 @@
+package hardingllc.pockethinman_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PanAndResizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pan_and_resize);
+    }
+}
